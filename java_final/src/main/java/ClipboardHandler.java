dbea337b590765c89ef2da666deaa64dbe11aaa4
mmienmien <95028987+mmienmien@users.noreply.github.com>
2022-06-03 22:58:25 +0800

    Add files via upload

--- java_final/src/main/java/ClipboardHandler.java
+++ java_final/src/main/java/ClipboardHandler.java
@@ -8,32 +8,42 @@ public class ClipboardHandler implements ClipboardOwner {
         void  onCopy(String data);
     }
     private Clipboard clipboard=Toolkit.getDefaultToolkit().getSystemClipboard();
+    private Transferable transferable;
     private  EntryListener entryListener;
+
+    private boolean toTranslet=false;
+    public boolean transletable(){
+        return toTranslet;
+    }
     public void  setEntryListener(EntryListener entryListener){
         this.entryListener=entryListener;
     }
-    @Override
-    public void lostOwnership(Clipboard clipboard,Transferable content){
+    @Override  public void lostOwnership(Clipboard clipboard,Transferable content){
         try{
             Thread.sleep(200);
             System.out.println((String) this.clipboard.getData(DataFlavor.stringFlavor));
+            toTranslet=true;
         }catch (Exception e){
             e.printStackTrace();
+            toTranslet=false;
         }
         Transferable cont=this.clipboard.getContents(this);
-        processContents(cont);
+        if(processContents(cont))toTranslet=true;
         regainOwnership(cont);
 
     }
-    public void processContents(Transferable cont){
+
+    public boolean processContents(Transferable cont){
         try{
             //Transferable co=clipboard.getContents(this);
             String r=(String) cont.getTransferData(DataFlavor.stringFlavor);
             if(entryListener!=null){
                 entryListener.onCopy(r);
             }
+            return true;
         }catch (Exception e){
             e.printStackTrace();
+            return false;
         }
     }
 
@@ -44,9 +54,13 @@ public class ClipboardHandler implements ClipboardOwner {
     public void  run(){
         Transferable t=clipboard.getContents(this);
         regainOwnership(t);
-        while (true);
+        //while (true);
     }
 
+    public void clearClipboard(){
+        StringSelection clear=new StringSelection("");
+        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(clear,this);
+    }
 
 
 }
